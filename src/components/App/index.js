124663ff3682e7e59a91067950fbce5c9577e820
Allenlixin <allenxinli@gmail.com>
2020-05-09 13:04:16 +0800

    Add header to my react order

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
+import Header from "../Header";
 import OrderList from "../OrderList";
 
 function App() {
   return (
     <div className="app">
+      <Header></Header>
       <OrderList></OrderList>
     </div>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import "./style.css";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return <header className="header">我的订单</header>;
   }
 }
 
