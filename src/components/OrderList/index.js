f1dc4961a6378ae500ce93bc0613ea38bc6ae715
Allenlixin <allenxinli@gmail.com>
2020-05-09 17:19:01 +0800

    Simulate fetch data

--- src/components/OrderList/index.js
+++ src/components/OrderList/index.js
@@ -1,50 +1,28 @@
 import React, { Component } from "react";
 import OrderItem from "../OrderItem";
 
-const data = [
-  {
-    id: 1,
-    shop: "院落创意菜",
-    picture:
-      "https://resources.matcha-jp.com/resize/720x2000/2018/05/24-54791.jpeg",
-    product: "百香果冷饮",
-    price: "19.9",
-    ifCommented: true,
-  },
-  {
-    id: 2,
-    shop: "院落创意菜",
-    picture:
-      "https://resources.matcha-jp.com/resize/720x2000/2018/05/24-54791.jpeg",
-    product: "百香果冷饮",
-    price: "19.9",
-    ifCommented: true,
-  },
-  {
-    id: 3,
-    shop: "院落创意菜",
-    picture:
-      "https://resources.matcha-jp.com/resize/720x2000/2018/05/24-54791.jpeg",
-    product: "百香果冷饮",
-    price: "19.9",
-    ifCommented: true,
-  },
-  {
-    id: 4,
-    shop: "院落创意菜",
-    picture:
-      "https://resources.matcha-jp.com/resize/720x2000/2018/05/24-54791.jpeg",
-    product: "百香果冷饮",
-    price: "19.9",
-    ifCommented: true,
-  },
-];
-
 class OrderList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] };
+  }
+
+  componentDidMount() {
+    fetch("/public/mock/orders.json").then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          this.setState({
+            data,
+          });
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <div>
-        {data.map((item) => {
+        {this.state.data.map((item) => {
           return <OrderItem key={item.id} data={item}></OrderItem>;
         })}
       </div>
